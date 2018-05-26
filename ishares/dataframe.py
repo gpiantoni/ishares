eb2714ffb053a042ffe4a5ad2b7866999c30f26e
Gio on ArchXPS <github@gpiantoni.com>
2018-05-26 07:10:50 +0200

    dataframe as class

--- ishares/dataframe.py
+++ ishares/dataframe.py
@@ -19,23 +19,34 @@ MONTHS = {
 }
 
 
-def dutch_date(s):
-    d, m, y = s.split('/')
-    return date(int(y), MONTHS[m], int(d))
-
 class WorkSheets():
     def __init__(self, raw):
         self.raw = _fix_xls(raw)
         self.root = ET.fromstring(self.raw)
-        self.ns = self.root.tag[:-8]
+        self.ns = self.root.tag[:-8]  # TODO: find a better way
+
+    def read_worksheet(self, name):
+        table = self._get_worksheet(name)
+
+        worksheet = []
+        for row in table:
+            one_row = []
+            for cell in row:
+                one_row.append(next(iter(cell)).text)
+
+            worksheet.append(one_row)
+
+        return _convert_to_df(worksheet, name)
 
     def _get_worksheet(self, name):
-        for worksheet in sel.froot:
+        """Historish"""
+        for worksheet in self.root:
             if worksheet.tag == self.ns + 'Worksheet':
-                if worksheet.attrib[ns + 'Name'] == 'Historisch':
+                if worksheet.attrib[self.ns + 'Name'] == name:
                     break
 
-        table = next(iter(worksheet))
+        return next(iter(worksheet))
+
 
 def _fix_xls(raw):
     raw = raw.replace('&euml;', 'ë')
@@ -43,22 +54,21 @@ def _fix_xls(raw):
     return raw
 
 
-def read_ishares(d):
-
-
-
-    worksheets = []
-    for row in table:
-        one_row = []
-        for cell in row:
-            one_row.append(next(iter(cell)).text)
-
-        worksheets.append(one_row)
-
-    df = DataFrame(worksheets)
+def _convert_to_df(worksheet, name):
+    df = DataFrame(worksheet)
     df.columns = df.iloc[0]
     df = df.reindex(df.index.drop(0))
-    df['Per'] = df['Per'].apply(dutch_date)
-    cols = list(df)[2:]
-    df[cols] = df[cols].apply(to_numeric, errors='coerce', axis=1)
+
+    if name == 'Historisch':
+        DATE_COLS = ['Per', ]
+        FLOAT_COLS = [list(df)[2:]]
+
+    df[DATE_COLS] = df[DATE_COLS].apply(_to_date)
+    df[FLOAT_COLS] = df[FLOAT_COLS].apply(to_numeric, errors='coerce', axis=1)
+
     return df
+
+
+def _to_date(s):
+    d, m, y = s.split('/')
+    return date(int(y), MONTHS[m], int(d))
