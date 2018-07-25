ace495d7690ba271dfe511483b11ed79e8825642
Gio Piantoni <github@gpiantoni.com>
2018-07-25 08:55:38 +0200

    show more etf

--- ishares/report_all.py
+++ ishares/report_all.py
@@ -42,7 +42,7 @@ def report_ishares(df):
     degiro_isin = list(DEGIRO['ISIN'])
     idx = (
         df['ISIN'].isin(degiro_isin)
-        & (df['TER'] <= 0.3))
+        & (df['TER'] <= 0.5))
     torst = df.loc[idx, COLS].sort_values('TER')
 
     torst['ISIN'] = torst.apply(lambda x: f'`{x["ISIN"]} <{x["Code"]}_{x["ISIN"]}.html>`_', axis=1)
