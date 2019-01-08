cbf090884a47f6dd6c16426484f02fbf591d60c7
Gio at UMCU <github@gpiantoni.com>
2019-01-08 11:16:34 +0100

    use html.parser

--- ishares/scrape.py
+++ ishares/scrape.py
@@ -19,7 +19,7 @@ def download_fund(id_fund):
 
     # parse html page
     resp_page = SESSION.get(SINGLE_FUND_URL.format(id_fund=id_fund))
-    soup_page = BeautifulSoup(resp_page.content.decode())
+    soup_page = BeautifulSoup(resp_page.content.decode(), features='html.parser')
     a_href = soup_page.find("a", {'data-link-event': "fund download:common"})
     download_url = urljoin(resp_page.url, a_href.attrs['href'])
 
